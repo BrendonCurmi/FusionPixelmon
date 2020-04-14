73c8ad25de6cc5adf714310a7eb94e226aec5900
BrendonCurmi <brendoncrm@gmail.com>
2020-04-14 18:07:57 +0200

    Hotfix to v1.5

--- src/main/java/me/FusionDev/FusionPixelmon/PluginInfo.java
+++ src/main/java/me/FusionDev/FusionPixelmon/PluginInfo.java
@@ -3,7 +3,7 @@ package me.FusionDev.FusionPixelmon;
 public class PluginInfo {
     public static final String ID = "fusionpixelmon";
     public static final String NAME = "FusionPixelmon";
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.5";
 
     public static final String CMD_PERM = ID + ".command.";
 }
