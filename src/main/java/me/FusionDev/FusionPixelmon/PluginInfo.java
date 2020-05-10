e97ded522d99334a17050575e93a9720f1f3b07f
BrendonCurmi <brendoncrm@gmail.com>
2020-05-11 01:36:21 +0200

    Upgrade plugin info to v1.8

--- src/main/java/me/FusionDev/FusionPixelmon/PluginInfo.java
+++ src/main/java/me/FusionDev/FusionPixelmon/PluginInfo.java
@@ -3,7 +3,7 @@ package me.FusionDev.FusionPixelmon;
 public class PluginInfo {
     public static final String ID = "fusionpixelmon";
     public static final String NAME = "FusionPixelmon";
-    public static final String VERSION = "1.7";
+    public static final String VERSION = "1.8";
 
     public static final String CMD_PERM = ID + ".command.";
 }
