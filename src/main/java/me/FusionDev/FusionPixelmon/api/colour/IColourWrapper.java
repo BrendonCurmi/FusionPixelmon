6184101f0e9a61da8497fff9be099ff3e1a458ff
BrendonCurmi <brendoncrm@gmail.com>
2020-05-28 16:17:48 +0200

    Remove src

--- src/main/java/me/FusionDev/FusionPixelmon/api/colour/IColourWrapper.java
+++
@@ -1,9 +0,0 @@
-package me.FusionDev.FusionPixelmon.api.colour;
-
-public interface IColourWrapper {
-    boolean hasColour();
-    void setColour(Colour colour);
-    boolean hasStyle();
-    void setStyle(Colour style);
-    String getFullCode();
-}
--- src/main/java/me/FusionDev/FusionPixelmon/api/updater/IUpdateChecker.java
+++
@@ -1,7 +0,0 @@
-package me.FusionDev.FusionPixelmon.api.updater;
-
-import java.io.IOException;
-
-public interface IUpdateChecker {
-    void check(String endpoint, String url) throws IOException;
-}
