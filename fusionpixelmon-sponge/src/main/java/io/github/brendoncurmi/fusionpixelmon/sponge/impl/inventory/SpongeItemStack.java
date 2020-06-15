1b92ee6aa37c18f35372f772b4ee2323d0a322aa
BrendonCurmi <brendoncrm@gmail.com>
2020-06-15 19:33:13 +0200

    Add functionality to SpongeItemStack

--- fusionpixelmon-sponge/src/main/java/io/github/brendoncurmi/fusionpixelmon/sponge/impl/inventory/SpongeItemStack.java
+++ fusionpixelmon-sponge/src/main/java/io/github/brendoncurmi/fusionpixelmon/sponge/impl/inventory/SpongeItemStack.java
@@ -1,19 +1,18 @@
 package io.github.brendoncurmi.fusionpixelmon.sponge.impl.inventory;
 
-import io.github.brendoncurmi.fusionpixelmon.api.Raw;
 import io.github.brendoncurmi.fusionpixelmon.api.items.AbstractItemStack;
 import io.github.brendoncurmi.fusionpixelmon.api.items.AbstractItemType;
-import org.spongepowered.api.data.key.Key;
 import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.data.value.BaseValue;
+import org.spongepowered.api.data.type.DyeColor;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.text.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpongeItemStack extends AbstractItemStack {
 
-    ItemStack itemStack;
+    private ItemStack itemStack;
 
     public SpongeItemStack(ItemStack itemStack) {
         this.itemStack = itemStack;
@@ -24,25 +23,48 @@ public class SpongeItemStack extends AbstractItemStack {
         return null;
     }
 
-
     @Override
     public void setName(String name) {
-
+        itemStack.offer(Keys.DISPLAY_NAME, Text.of(name));
     }
 
     @Override
     public List<String> getLore() {
-        return null;
+        if (itemStack.get(Keys.ITEM_LORE).isPresent()) {
+            return toStringList(itemStack.get(Keys.ITEM_LORE).get());
+        }
+        return new ArrayList<>();
     }
 
     @Override
     public void setLore(List<String> itemLore) {
+        itemStack.offer(Keys.ITEM_LORE, toTextList(itemLore));
+    }
 
+    @Override
+    public void setColour(Object colour) {
+        itemStack.offer(Keys.DYE_COLOR, (DyeColor) colour);
     }
 
     @Override
     public ItemStack getRaw() {
         //return ItemStack.builder().fromItemStack(itemStack).add(Keys.DISPLAY_NAME, Text.of(name)).build();
         return ItemStack.builder().fromItemStack(itemStack).build();
+    }
+
+    private List<Text> toTextList(List<String> list) {
+        List<Text> lore = new ArrayList<>();
+        for (String string : list) {
+            lore.add(Text.of(string));
+        }
+        return lore;
+    }
+
+    private List<String> toStringList(List<Text> list) {
+        List<String> lore = new ArrayList<>();
+        for (Text text : list) {
+            lore.add(text.toPlain());
+        }
+        return lore;
     }
 }
