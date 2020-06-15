46cdf8f3478a10eb97d0215e68d67eca6ca1906d
BrendonCurmi <brendoncrm@gmail.com>
2020-06-15 19:33:22 +0200

    Add functionality to SpongeItemType

--- fusionpixelmon-sponge/src/main/java/io/github/brendoncurmi/fusionpixelmon/sponge/impl/inventory/SpongeItemType.java
+++ fusionpixelmon-sponge/src/main/java/io/github/brendoncurmi/fusionpixelmon/sponge/impl/inventory/SpongeItemType.java
@@ -3,20 +3,19 @@ package io.github.brendoncurmi.fusionpixelmon.sponge.impl.inventory;
 import io.github.brendoncurmi.fusionpixelmon.api.items.AbstractItemStack;
 import io.github.brendoncurmi.fusionpixelmon.api.items.AbstractItemType;
 import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.inventory.ItemStack;
 
 public class SpongeItemType extends AbstractItemType {
 
-    ItemType itemType;
+    private ItemType itemType;
 
     public SpongeItemType(ItemType itemType) {
         this.itemType = itemType;
     }
 
-
-
     @Override
     public AbstractItemStack to() {
-        return null;
+        return new SpongeItemStack(ItemStack.builder().itemType(itemType).build());
     }
 
     @Override
