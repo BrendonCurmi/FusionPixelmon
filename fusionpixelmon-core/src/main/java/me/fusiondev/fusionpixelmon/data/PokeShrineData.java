ade84e59eb0dbff4147de3b3704a2c61ce7a6fc2
BrendonCurmi <brendoncrm@gmail.com>
2020-08-07 18:48:56 +0200

    Create PokeShrineData data class

---
+++ fusionpixelmon-core/src/main/java/me/fusiondev/fusionpixelmon/data/PokeShrineData.java
@@ -0,0 +1,47 @@
+package me.fusiondev.fusionpixelmon.data;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.UUID;
+
+public class PokeShrineData extends Data {
+
+    public PokeShrineData(File file, String name) {
+        super(file, name);
+    }
+
+    /**
+     * Builds the default structure of the data.
+     *
+     * @return the data JSONObject.
+     */
+    @Override
+    protected JSONObject build() {
+        return new JSONObject();
+    }
+
+    private String getKey(int x, int y, int z) {
+        return x + "," + y + "," + z;
+    }
+
+    public void lock(int x, int y, int z, UUID uuid) {
+        String key = getKey(x, y, z);
+        data.put(key, uuid);
+    }
+
+    public void unlock(int x, int y, int z) {
+        String key = getKey(x, y, z);
+        data.remove(key);
+    }
+
+    public boolean isLocked(int x, int y, int z) {
+        String key = getKey(x, y, z);
+        return data.has(key);
+    }
+
+    public boolean isLockedBy(int x, int y, int z, UUID uuid) {
+        String key = getKey(x, y, z);
+        return data.has(key) && data.get(key) == uuid;
+    }
+}
