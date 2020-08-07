8f5787933a1fcdcc6270e5c7afb8ca3982af2021
BrendonCurmi <brendoncrm@gmail.com>
2020-08-07 18:50:58 +0200

    Implement pokeshrine locking in Spigot

--- fusionpixelmon-spigot/src/main/java/me/fusiondev/fusionpixelmon/spigot/modules/pokeshrines/SpigotPokeShrines.java
+++ fusionpixelmon-spigot/src/main/java/me/fusiondev/fusionpixelmon/spigot/modules/pokeshrines/SpigotPokeShrines.java
@@ -2,11 +2,13 @@ package me.fusiondev.fusionpixelmon.spigot.modules.pokeshrines;
 
 import me.fusiondev.fusionpixelmon.FusionPixelmon;
 import me.fusiondev.fusionpixelmon.api.colour.Colour;
+import me.fusiondev.fusionpixelmon.data.PokeShrineData;
 import me.fusiondev.fusionpixelmon.modules.pokeshrines.PokeShrinesModule;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -20,31 +22,82 @@ public class SpigotPokeShrines extends PokeShrinesModule implements Listener {
      */
     private static final Material EMPTY_BLOCK = Material.AIR;
 
+    private final PokeShrineData DATA;
+
+    public SpigotPokeShrines(PokeShrineData data) {
+        this.DATA = data;
+    }
+
     @EventHandler
     public void onBlockClick(PlayerInteractEvent event) {
         if ((event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.LEFT_CLICK_BLOCK))
                 && event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
-            String block = event.getClickedBlock().getType().toString().toLowerCase().replace("pixelmon_", "pixelmon:");
+            String block = fix(event.getClickedBlock().getType().toString());
+            Player player = event.getPlayer();
             if (BLOCKS.containsKey(block) && isAllowedTool(BLOCKS.get(block), event.getMaterial().toString())) {
-                if (event.getPlayer().getInventory().firstEmpty() != -1) {
-                    block = event.getClickedBlock().getType().toString();
-                    ItemStack selected = (ItemStack) FusionPixelmon.getRegistry().getPixelmonUtils().getPixelmonItemType(block.toLowerCase().replace("pixelmon_", "")).to().getRaw();
+                if (player.getInventory().firstEmpty() != -1) {
+                    Location blockLoc = getLocation(event.getClickedBlock().getLocation(), block);
+                    int x = blockLoc.getBlockX(), y = blockLoc.getBlockY(), z = blockLoc.getBlockZ();
+                    if (DATA.isLocked(x, y, z)) {
+                        if (!DATA.isLockedBy(x, y, z, player.getUniqueId())) {
+                            player.sendMessage("§cThis shrine is locked for pickup by another player");
+                            return;
+                        }
+                        DATA.unlock(x, y, z);
+                    }
+
+                    ItemStack selected = (ItemStack) FusionPixelmon.getRegistry().getPixelmonUtils().getPixelmonItemType(block.toLowerCase().replace("pixelmon:", "")).to().getRaw();
 
                     event.getClickedBlock().setType(EMPTY_BLOCK);
 
                     // Some blocks may have a secondary block to make it bigger, so remove that too
                     Block clickedBlock = event.getClickedBlock();
                     Location block2Loc = new Location(clickedBlock.getWorld(), clickedBlock.getX(), clickedBlock.getY() + 1, clickedBlock.getZ());
-                    if (!block2Loc.getBlock().getType().name().equalsIgnoreCase(block)) {
+                    if (!fix(block2Loc.getBlock().getType().name()).equalsIgnoreCase(block)) {
                         block2Loc = new Location(clickedBlock.getWorld(), clickedBlock.getX(), clickedBlock.getY() - 1, clickedBlock.getZ());
                     }
-                    if (block2Loc.getBlock().getType().name().equalsIgnoreCase(block)) {
+                    if (fix(block2Loc.getBlock().getType().name()).equalsIgnoreCase(block)) {
                         block2Loc.getBlock().setType(EMPTY_BLOCK);
                     }
 
-                    event.getPlayer().getInventory().addItem(selected);
-                } else event.getPlayer().sendMessage("§" + Colour.RED.getCode() + "Your inventory is full!");
+                    player.getInventory().addItem(selected);
+                } else player.sendMessage("§" + Colour.RED.getCode() + "Your inventory is full!");
+            } else if (BLOCKS.containsKey(block) && !event.hasItem()) {
+                Location blockLoc = getLocation(event.getClickedBlock().getLocation(), block);
+                int x = blockLoc.getBlockX(), y = blockLoc.getBlockY(), z = blockLoc.getBlockZ();
+                if (DATA.isLocked(x, y, z)) {
+                    if (DATA.isLockedBy(x, y, z, player.getUniqueId())) {
+                        DATA.unlock(x, y, z);
+                        player.sendMessage("§aThis shrine has been unlocked for pickup");
+                    } else {
+                        player.sendMessage("§cThis shrine is already locked for pickup by another player");
+                    }
+                } else {
+                    DATA.lock(x, y, z, player.getUniqueId());
+                    player.sendMessage("§aThis shrine has been locked for pickup");
+                }
             }
         }
+    }
+
+    /**
+     * Gets the lowest possible location for the specified shrine block,
+     * as some shrine blocks may have a secondary block to make it bigger.
+     *
+     * @param location the clicked block location.
+     * @param block    the shrine block.
+     * @return the lowest location of the shrine block.
+     */
+    private Location getLocation(Location location, String block) {
+        Location blockLoc = location;
+        Location block2Loc = new Location(blockLoc.getWorld(), blockLoc.getBlockX(), blockLoc.getBlockY() - 1, blockLoc.getBlockZ());
+        if (fix(block2Loc.getBlock().getType().name()).equalsIgnoreCase(block)) {
+            blockLoc = block2Loc;
+        }
+        return blockLoc;
+    }
+
+    private static String fix(String s) {
+        return s.toLowerCase().replace("pixelmon_", "pixelmon:");
     }
 }
