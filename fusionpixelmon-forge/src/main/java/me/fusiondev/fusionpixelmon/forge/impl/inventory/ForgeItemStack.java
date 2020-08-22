2f8eaa493f04b813bfc7b0f16e36b3cfd856cd1a
BrendonCurmi <brendoncrm@gmail.com>
2020-08-23 00:17:05 +0200

    Implement setColour in ForgeItemStack

--- fusionpixelmon-forge/src/main/java/me/fusiondev/fusionpixelmon/forge/impl/inventory/ForgeItemStack.java
+++ fusionpixelmon-forge/src/main/java/me/fusiondev/fusionpixelmon/forge/impl/inventory/ForgeItemStack.java
@@ -1,7 +1,11 @@
 package me.fusiondev.fusionpixelmon.forge.impl.inventory;
 
+import me.fusiondev.fusionpixelmon.api.colour.DyeColor;
 import me.fusiondev.fusionpixelmon.api.items.AbstractItemStack;
 import me.fusiondev.fusionpixelmon.api.items.AbstractItemType;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
@@ -41,6 +45,21 @@ public class ForgeItemStack extends AbstractItemStack {
             }
         }
         ITEMSTACK.getOrCreateSubCompound("display").setTag("Lore", nbtLore);
+        return this;
+    }
+
+    @Override
+    public AbstractItemStack setColour(Object colour) {
+        if (colour instanceof DyeColor) {
+            if (ITEMSTACK.getItem() == Items.DYE
+                    || ITEMSTACK.getItem() == Items.BANNER
+                    || ITEMSTACK.getItem() == Item.getItemFromBlock(Blocks.STANDING_BANNER)
+                    || ITEMSTACK.getItem() == Item.getItemFromBlock(Blocks.WALL_BANNER)) {
+                ITEMSTACK.setItemDamage(((DyeColor) colour).getDyeData());
+            } else {
+                ITEMSTACK.setItemDamage(((DyeColor) colour).getBlockData());
+            }
+        } else ITEMSTACK.setItemDamage((byte) colour);
         return this;
     }
 
