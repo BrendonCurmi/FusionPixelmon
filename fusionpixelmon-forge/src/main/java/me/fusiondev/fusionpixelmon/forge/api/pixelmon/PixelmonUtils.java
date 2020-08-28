112c2558ecb54b0a555e013f01fda95ed6960b84
BrendonCurmi <brendoncrm@gmail.com>
2020-08-28 11:05:35 +0200

    Remove pixelmon items tooltips from Forge

--- fusionpixelmon-forge/src/main/java/me/fusiondev/fusionpixelmon/forge/api/pixelmon/PixelmonUtils.java
+++ fusionpixelmon-forge/src/main/java/me/fusiondev/fusionpixelmon/forge/api/pixelmon/PixelmonUtils.java
@@ -7,6 +7,8 @@ import me.fusiondev.fusionpixelmon.api.items.AbstractItemType;
 import me.fusiondev.fusionpixelmon.api.pixelmon.AbstractPixelmonUtils;
 import me.fusiondev.fusionpixelmon.forge.ForgeAdapter;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 
 public class PixelmonUtils extends AbstractPixelmonUtils {
@@ -23,5 +25,19 @@ public class PixelmonUtils extends AbstractPixelmonUtils {
     @Override
     public AbstractItemType getPixelmonItemType(String id) {
         return ForgeAdapter.adapt(Item.REGISTRY.getObject(new ResourceLocation("pixelmon", id)));
+    }
+
+    @Override
+    public AbstractItemStack getPixelmonItemStack(String id) {
+        Item item = Item.REGISTRY.getObject(new ResourceLocation("pixelmon", id));
+        if (item == null) throw new IllegalArgumentException("Cant find pixelmon item '" + id + "'");
+        ItemStack itemStack = new ItemStack(item);
+        // Remove pixelmon item tooltip
+        if (itemStack.getTagCompound() == null) {
+            NBTTagCompound tag = new NBTTagCompound();
+            tag.setString("tooltip", "");
+            itemStack.setTagCompound(tag);
+        }
+        return ForgeAdapter.adapt(itemStack);
     }
 }
