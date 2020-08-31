ae0d2475c6856ed72574ae5560cf6c3e3514148a
BrendonCurmi <brendoncrm@gmail.com>
2020-08-31 21:37:10 +0200

    Add check for player command source for PokeDesignerCommand

--- fusionpixelmon-spigot/src/main/java/me/fusiondev/fusionpixelmon/spigot/modules/pokedesigner/commands/PokeDesignerCommand.java
+++ fusionpixelmon-spigot/src/main/java/me/fusiondev/fusionpixelmon/spigot/modules/pokedesigner/commands/PokeDesignerCommand.java
@@ -15,16 +15,18 @@ import org.bukkit.entity.Player;
 public class PokeDesignerCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (commandSender instanceof Player) {
-            Player player = (Player) commandSender;
-            Shops shops = new SpigotShops(SpigotAdapter.adapt(player));
-            PokeDesignerConfig config = FusionPixelmon.getInstance().getConfiguration().getPokeDesignerConfig();
-            new PokeSelectorUI(SpigotAdapter.adapt(player), config.getPokeSelectorGuiTitle(), "pokeselector", pokemon -> {
-                if (!config.containsBlackListedPokemon(pokemon.getSpecies())) {
-                    shops.launch(pokemon, config.getGuiTitle());
-                } else player.sendMessage(Color.RED + "That Pokemon cant use the PokeDesigner!");
-            });
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage(Color.RED + "This command can only be executed by a player");
+            return true;
         }
+        Player player = (Player) commandSender;
+        Shops shops = new SpigotShops(SpigotAdapter.adapt(player));
+        PokeDesignerConfig config = FusionPixelmon.getInstance().getConfiguration().getPokeDesignerConfig();
+        new PokeSelectorUI(SpigotAdapter.adapt(player), config.getPokeSelectorGuiTitle(), "pokeselector", pokemon -> {
+            if (!config.containsBlackListedPokemon(pokemon.getSpecies())) {
+                shops.launch(pokemon, config.getGuiTitle());
+            } else player.sendMessage(Color.RED + "That Pokemon cant use the PokeDesigner!");
+        });
         return true;
     }
 }
