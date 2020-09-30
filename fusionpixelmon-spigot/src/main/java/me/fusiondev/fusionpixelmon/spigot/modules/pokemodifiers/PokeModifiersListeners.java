21327283dad16220001b44dde7cb6fa8503baf26
BrendonCurmi <brendoncrm@gmail.com>
2020-09-30 15:21:56 +0200

    Create PokeModifiers event listener in Spigot

---
+++ fusionpixelmon-spigot/src/main/java/me/fusiondev/fusionpixelmon/spigot/modules/pokemodifiers/PokeModifiersListeners.java
@@ -0,0 +1,28 @@
+package me.fusiondev.fusionpixelmon.spigot.modules.pokemodifiers;
+
+import me.fusiondev.fusionpixelmon.modules.pokemodifiers.PokeModifiers;
+import me.fusiondev.fusionpixelmon.spigot.SpigotAdapter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
+public class PokeModifiersListeners implements Listener {
+    @EventHandler
+    public void onClick(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        ItemStack itemStack = player.getInventory().getItemInMainHand();
+        if (!itemStack.hasItemMeta()) return;
+        String name = itemStack.getItemMeta().getDisplayName();
+        if (event.getHand() == EquipmentSlot.HAND && !name.isEmpty() && PokeModifiers.hasModifier(name, true)) {
+            PokeModifiers.getModifier(name, true).action(SpigotAdapter.adapt(player), () -> {
+                if (itemStack.getAmount() > 1)
+                    itemStack.setAmount(itemStack.getAmount() - 1);
+                else
+                    player.getInventory().remove(itemStack);
+            });
+        }
+    }
+}
