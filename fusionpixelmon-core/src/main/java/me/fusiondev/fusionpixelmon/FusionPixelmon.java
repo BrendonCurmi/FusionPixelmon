96d5b83129e2c161f74ff1ffb7528a1058e74d83
BrendonCurmi <brendoncrm@gmail.com>
2020-10-12 20:04:39 +0200

    Update FusionPixelmon

--- fusionpixelmon-core/src/main/java/me/fusiondev/fusionpixelmon/FusionPixelmon.java
+++ fusionpixelmon-core/src/main/java/me/fusiondev/fusionpixelmon/FusionPixelmon.java
@@ -1,6 +1,8 @@
 package me.fusiondev.fusionpixelmon;
 
 public class FusionPixelmon {
+    //todo should make form, pokeball shops autopage
+
     public static final String VERSION = "1.9";
 
     public static Registry registry;
